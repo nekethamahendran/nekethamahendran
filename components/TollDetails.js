5c2d7659429000f97934bf56e0f0707ae5bd07bb
nekethamahendran <141517303+nekethamahendran@users.noreply.github.com>
2023-12-09 21:00:00 +0530

    Create TollDetails.js

---
+++ components/TollDetails.js
@@ -0,0 +1,13 @@
+// components/TollDetails.js
+import React from 'react';
+
+function TollDetails() {
+  return (
+    <div>
+      {/* Integrate toll details on the markers along the route */}
+      {/* Display relevant toll information, such as cost and additional details */}
+    </div>
+  );
+}
+
+export default TollDetails;
