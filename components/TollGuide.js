dc90f371f7c8e5c8da95a007c1c62152ded35413
nekethamahendran <141517303+nekethamahendran@users.noreply.github.com>
2023-12-09 20:57:11 +0530

    Create TollGuide.js

---
+++ components/TollGuide.js
@@ -0,0 +1,13 @@
+// components/TollGuide.js
+import React from 'react';
+
+function TollGuide() {
+  return (
+    <div>
+      {/* Implement an interactive guide or tooltip system */}
+      <p>Learn how toll calculations are performed...</p>
+    </div>
+  );
+}
+
+export default TollGuide;
